d3bb7f37494ffff6dbe8aecc98233c6c8ef50660
Candy Frederiksen <candic3@pdx.edu>
2019-11-06 16:11:45 -0800

    Java Attempt

---
+++ project-two-template.js
@@ -0,0 +1,21 @@
+
+
+$( document ).ready(function() {
+   
+
+     	$("#navbar_visible").hide();
+
+});
+
+
+ $(document).on("click","#navbar",function() {
+
+ 	$("#navbar_visible").show();
+ 	
+
+
+
+
+
+
+ });
