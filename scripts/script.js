05a98e82996091a27485228003030ccc90cd72bd
Candy Frederiksen <candic3@pdx.edu>
2019-11-18 14:47:16 -0800

    Changed Nav and Added Hover Effects

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,21 @@
 $(function() {
 // Your Code from here on down. Don't delete that line above!
   
+/*
+  Slidemenu
+*/
+(function() {
+	var $body = document.body
+	, $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
+
+	if ( typeof $menu_trigger !== 'undefined' ) {
+		$menu_trigger.addEventListener('click', function() {
+			$body.className = ( $body.className == 'menu-active' )? '' : 'menu-active';
+		});
+	}
+
+}).call(this);
+
 
 
 
