87d1948beacd0db305698f23b6f9ac369817408e
Candy Frederiksen <candic3@pdx.edu>
2019-11-18 15:08:22 -0800

    Added Bass Strings to Work!

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,27 @@
 $(function() {
 // Your Code from here on down. Don't delete that line above!
   
+
+  $('.e-string').click(function() {
+  	$('.e-string').attr('fill', 'purple')
+
+  	})
+
+  	$('.g-string').click(function() {
+  	$('.g-string').attr('fill', 'red')
+
+  	})
+
+  	$('.d-string').click(function() {
+  	$('.d-string').attr('fill', 'green')
+
+  	})
+
+  	$('.a-string').click(function() {
+  	$('.a-string').attr('fill', 'blue')
+
+  	})
+  
 /*
   Slidemenu
 */
