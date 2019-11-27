0b58040316a64639c076e762a339697dfc4ed968
Candy Frederiksen <candic3@pdx.edu>
2019-11-27 13:50:34 -0800

    KEY SOUNDS!

--- scripts/script.js
+++ scripts/script.js
@@ -6,8 +6,77 @@ $(function() {
 
 
 
-    $('.ding').click(function() {
-        $('.audio#key')[0].play()
+    $('.ding').mouseover(function() {
+        $('audio#key')[0].play()
+
+    })
+
+    $('.ding').mouseleave(function() {
+        $('audio#key')[0].currentTime = 0
+
+    })
+
+    
+
+    $('.dingb').mouseover(function() {
+        $('audio#keyb')[0].play()
+
+    })
+
+    $('.dingb').mouseleave(function() {
+        $('audio#keyb')[0].currentTime = 0
+
+    })
+
+    
+
+
+    $('.dingc').mouseover(function() {
+        $('audio#keyc')[0].play()
+
+    })
+
+   	$('.dingc').mouseleave(function() {
+        $('audio#keyc')[0].currentTime = 0
+
+    })
+
+
+
+
+     $('.dingd').mouseover(function() {
+        $('audio#keyd')[0].play()
+
+    })
+
+   	$('.dingd').mouseleave(function() {
+        $('audio#keyd')[0].currentTime = 0
+
+    })
+
+
+
+
+     $('.dinge').mouseover(function() {
+        $('audio#keye')[0].play()
+
+    })
+
+   	$('.dinge').mouseleave(function() {
+        $('audio#keye')[0].currentTime = 0
+
+    })
+
+
+
+
+     $('.dingf').mouseover(function() {
+        $('audio#keyf')[0].play()
+
+    })
+
+   	$('.dingf').mouseleave(function() {
+        $('audio#keyf')[0].currentTime = 0
 
     })
 
